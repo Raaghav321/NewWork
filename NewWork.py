4ae82dbe0ea344216090d0b3d1692b6b7950ec82
Raaghav321 <rawasthi2005@gmail.com>
2019-10-07 09:02:30 -0500

    fhfhhf

--- NewWork.py
+++ NewWork.py
@@ -1,7 +1,11 @@
+# making a joke using variables.
+
 x= "There are %d types of people" %10
 binary="binary"
 doNot = "don't"
 y = "Those who know %s and those who %s" % (binary, doNot)
+
+# Actually making the joke.
 
 print(x)
 print(y)
@@ -12,6 +16,8 @@ print("I also said: '%s ." % y)
 hilarious = False
 joke_evalution = "isn't that funny?! %r"
 print(joke_evalution % hilarious)
+
+# making a sentence using variables and plugging them in.
 
 w="This is the left side of..."
 e="a string with a right side"
