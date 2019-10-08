0a8877f48e81378505a23a082198a5dae65c22f3
Raaghav321 <rawasthi2005@gmail.com>
2019-10-08 08:29:18 -0500

    Mary had a little lamb

--- NewWork.py
+++ NewWork.py
@@ -23,3 +23,25 @@ w="This is the left side of..."
 e="a string with a right side"
 
 print(w+e)
+
+# More stuff  10-8
+
+print("Mary had a little lamb")
+print("It's fleece was what as %s" % 'snow')
+print("And every where that mary went.")
+print("." * 10)
+
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1)
